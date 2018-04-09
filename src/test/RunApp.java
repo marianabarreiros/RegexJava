852f0eef40581bd23e083c37371ae8a24661fa90
Mariana Barreiros <maridepp1@hotmail.com>
2018-04-09 11:33:23 -0300

    finalizado

--- src/test/RunApp.java
+++ src/test/RunApp.java
@@ -15,17 +15,16 @@ import java.util.regex.Pattern;
  */
 public class RunApp {
     public static void main(String[] args) {
-//        ([a-zA-Z]+:)? -> não funciona trazer 'Reward:' uma única vez
-        String outra;
-        String regex = "([a-zA-Z]+:)?([0-9]{2})([a-zA-Z]{3})([0-9]{4})\\(([a-zA-Z]{3,4})\\)";
-        String string = "Rewards:26Mar2009(thur),27Mar2009(fri),28Mar2009(sat)";
+//      
+        String outra = "";
+        String regex = "([a-zA-Z]+:)?([0-9]{2})([a-zA-Z]{3})([0-9]{4})\\(([a-zA-Z]{3,4})\\)+";
+        String string = "Rewards:26Mar2009(thur),27Mar2009(fri),28Mar2009(sat),28Mar2009(sat),28Mar2009(sat)";
         Pattern padrao = Pattern.compile(regex);
         Matcher mat = padrao.matcher(string);
-//        +([(]{1})([a-zA-Z]{3})+([)]{1})
-//([(]{1})([a-zA-Z]{3})([)]{1})
         while(mat.find()){
-            System.out.println(mat.group());
+            outra+=(mat.group());    
         }
+        System.out.println(outra);
 //        Expressoes ex = new Expressoes("Rewards: 26Mar2009(thur), 27Mar2009(fri), 28Mar2009(sat)");
 //        System.out.println(ex.validaPadrao());
     }
